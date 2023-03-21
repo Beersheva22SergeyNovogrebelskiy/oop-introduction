d56348462d6c22a114b5eefae0568c5cfcb18c92
Sergey Novogrebelskiy <sznovo@yandex.ru>
2023-03-21 07:42:56 +0200

    Completed homework of 26 January.

--- src/telran/util/Collection.java
+++ src/telran/util/Collection.java
@@ -1,7 +1,10 @@
 package telran.util;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.function.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public interface Collection<T> extends Iterable<T>{
 boolean add (T element);
@@ -32,16 +35,37 @@ boolean contains(T pattern);
  * Collection will be put in the array and rest of memory will be filled by null's 
  */
  
-default T[] toArray(T[] ar) {
-	int size = size();
-	if (ar.length < size) {
-		ar = Arrays.copyOf(ar, size);
+default T[] toArray (T[] array) {
+	if (array.length < size()) {
+		array = Arrays.copyOf(array, size());
 	}
+	Iterator<T> it = iterator();
 	int index = 0;
-	for(T obj: this) {
-		ar[index++] = obj;
+	while(it.hasNext()) { 
+		array[index++] = it.next();		
 	}
-	Arrays.fill(ar, size, ar.length, null);
-	return ar;
+	Arrays.fill(array, size(), array.length, null);
+	return array;
 }
+
+default Stream<T> stream() {
+	return StreamSupport.stream(this.spliterator(), false);
+	}
+
+default Stream<T> parallelSrteam() {
+	return StreamSupport.stream(this.spliterator(), true);
+	}
+
+default T[] toArrayShuffling(T[] array) {
+	T[] res = array;
+	ArrayList <Integer> ind = new ArrayList<>();
+	new Random().ints(0,array.length).distinct().limit(array.length).forEach(ind::add);
+		for (int i = 0; i < array.length; i++) {
+		T temp = res[i];
+		res[i] = res[ind.get(i)];
+		res[ind.get(i)] = temp;	
+	}
+	return res;	
+}
+
 }
